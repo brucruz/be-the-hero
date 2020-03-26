cda3ddaa36d1f356cd68d286e462d544367a5ab4
Bruno Cruz <bru.cruz@gmail.com>
2020-03-25 22:55:23 -0300

    configurado o nodemon no npm start

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+// Informar ao Express para converter o arquivo JSON em objeto do JS
+app.use(express.json());
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query Params: Parâmetros nomeados enviados na rota após o símbolo de '?' (filtros, paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+  */
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
         evento: 'Semana Omnistack 11.0',
         aluno: 'Bruno Cruz',
