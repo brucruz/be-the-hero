443ba273001774a4b86b48c6a1dfc27da6d66909
Bruno Cruz <bru.cruz@gmail.com>
2020-03-27 00:56:36 -0300

    integradas as páginas Register e Logon às apis correspondentes

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api'
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+    // Armazenar estados dos inputs
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    // Guarda o histórico da navegação
+    const history = useHistory();
+    
+    // Função responsável por fazer o cadastro do usuário
+    async function handleRegister(e) {
+        // Prevenir o recarregamento da página, padrão
+        e.preventDefault();
+
+        // Armazenar dados do form
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        };
+
+        try {
+            const response = await api.post('ongs', data);
+
+            alert(`Seu ID de acesso: ${response.data.id}`)
+            
+            //Enviar o histórico para a rota raíz, de login
+            history.push('/');
+        } catch (err) {
+            alert('Erro no cadastro, tente novamente.');
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -22,14 +59,39 @@ export default function Register() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da ONG" />
-                    <input placeholder="E-mail" />
-                    <input placeholder="WhatsApp" />
+                <form onSubmit={handleRegister}>
+                    <input 
+                    placeholder="Nome da ONG"
+                    value={name}
+                    // Acompanhar as mudanças nesse input, pegar o evento de mudança e colocar na variável armazenada no estado
+                    onChange={e => setName(e.target.value)}
+                    />
+                    <input 
+                    placeholder="E-mail"
+                    value={email}
+                    // Acompanhar as mudanças nesse input, pegar o evento de mudança e colocar na variável armazenada no estado
+                    onChange={e => setEmail(e.target.value)}
+                    />
+                    <input 
+                    placeholder="WhatsApp"
+                    value={whatsapp}
+                    // Acompanhar as mudanças nesse input, pegar o evento de mudança e colocar na variável armazenada no estado
+                    onChange={e => setWhatsapp(e.target.value)}
+                    />
                     
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{ width: 80 }} />
+                        <input 
+                        placeholder="Cidade"
+                        value={city}
+                        // Acompanhar as mudanças nesse input, pegar o evento de mudança e colocar na variável armazenada no estado
+                        onChange={e => setCity(e.target.value)}
+                        />
+                        <input 
+                        placeholder="UF" style={{ width: 80 }}
+                        value={uf}
+                        // Acompanhar as mudanças nesse input, pegar o evento de mudança e colocar na variável armazenada no estado
+                        onChange={e => setUf(e.target.value)}
+                        />
                     </div>
 
                     <button className="button" type="submit">Cadastrar</button>
