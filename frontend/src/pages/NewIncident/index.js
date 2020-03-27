1fb3bd1b7ad8b22376face69dc05aab48abbf311
Bruno Cruz <bru.cruz@gmail.com>
2020-03-26 23:02:50 -0300

    criada página e rota NewIncident

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncident() {
+    return <h1>New Incident</h1>
+}
