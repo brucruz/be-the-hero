0a5d13eeaab05977f9e349a6c763c96060e71588
Bruno Cruz <bru.cruz@gmail.com>
2020-03-29 18:58:05 -0300

    configurado o ambiente de teste

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -5,8 +5,11 @@ const knex = require('knex');
 // Importar configurações do banco de dados do arquivo knexfile.js
 const configuration = require('../../knexfile')
 
+// se a variável ambiente for 'test', a configuração utilizada será configuration.test, se não será configuration.development
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development
+
 //Criar conexão usando o knex, passando o parâmetro de conexão de development
-const connection = knex(configuration.development);
+const connection = knex(config);
 
 // Exportar conexão com o banco de dados
 module.exports = connection;
